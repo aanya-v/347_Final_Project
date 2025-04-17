d196a8a35febefe54482f5ed89a25315caa1f7c4
keelygrice <keely.grice@emory.edu>
2025-04-17 16:50:34 -0400

    New dataset gym

--- Clean_Datasets.R
+++ Clean_Datasets.R
@@ -20,13 +20,13 @@ data_clean <- data %>%
 summary(data_clean)
 names(data_clean) <- c("Calories", "Age", "Gender", "Duration", "Heart_Rate", "Weight", "Height")
 
-#write.csv(data_clean, "calories2.csv", row.names=FALSE)
+write.csv(data_clean, "calories2.csv", row.names=FALSE)
 summary(cal)
 
 #Impute Body Temp
 #Rename variables to match
 df <- data_clean
-#names(df) <- c("Calories", "Age", "Gender", "Duration", "Heart_Rate", "Weight", "Height")
+names(df) <- c("Calories", "Age", "Gender", "Duration", "Heart_Rate", "Weight", "Height")
 
 #Add column for Body_Temp
 df$Body_Temp <- as.numeric(NA)
@@ -55,4 +55,52 @@ imputed[1, ]
 imputed_drop <- imputed %>%
   select(Gender, Age, Height, Weight, Duration, Heart_Rate, Body_Temp, Calories)
 
-#write.csv(imputed, "imputed.csv", row.names=FALSE)
+write.csv(imputed_drop, "imputed.csv", row.names=FALSE)
+
+summary(imputed_drop)
+
+#Clean gym dataset
+gym<- read.csv("gym_members_exercise_tracking.csv")
+
+summary(gym)
+gym <- gym %>%
+  select(Age, Gender, Weight..kg., Height..m., Avg_BPM, Session_Duration..hours., Calories_Burned)
+summary(gym)
+
+names(gym) <- c("Age", "Gender", "Weight", "Height", "Heart_Rate", "Duration", "Calories")
+
+#Gender to binary
+gym$Gender <- ifelse(gym$Gender == "Female", 1, 0)
+gym$Gender
+
+#Height units
+gym$Height <- gym$Height *100
+gym$Height
+
+#Duration in minutes
+gym$Duration <- gym$Duration*60
+
+#write.csv(gym, "gym_nonimputed.csv", row.names=FALSE)
+
+gym$Body_Temp <- NA
+
+#impute body temp
+#Combine datasets
+combined <- rbind(cal, gym)
+summary(combined)
+nrow(combined)
+
+# Impute missing values using kNN method
+imputed1 <- kNN(combined, k=20)
+summary(imputed$Body_Temp)
+
+combined[15000, ]
+combined[15001, ]
+
+imputed1 <- imputed1[-(1:15000), ]
+imputed1[1, ]
+
+imputed1_drop <- imputed1 %>%
+  select(Gender, Age, Height, Weight, Duration, Heart_Rate, Body_Temp, Calories)
+
+write.csv(imputed1_drop, "gym_imputed.csv", row.names=FALSE)
