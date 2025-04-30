e193de260e5fe2428f3518f6d8c0969d6574f319
keelygrice <keely.grice@emory.edu>
2025-04-30 13:06:05 -0400

    Added code comments

--- Datasets for Imputing/Clean_Datasets.R
+++ Datasets for Imputing/Clean_Datasets.R
@@ -1,90 +1,38 @@
-#Clean second dataset to use in models
+#Clean second dataset to check for generalizability
 library(dplyr)
 library(VIM)
 
-data <- read.csv("exercise_dataset.csv")
+#import datasets
 cal <- read.csv("calories.csv")
-
-#Turn gender into binary
-data$Gender <- ifelse(data$Gender == "Female", 1, 0)
-data$Gender
-
-#Calculate Height
-data$Actual.Weight
-data$Height <- 100*sqrt(data$Actual.Weight / data$BMI)
-data$Height
-
-data_clean <- data %>%
-  select(Calories.Burn, Age, Gender, Duration, Heart.Rate, Actual.Weight, Height)
-
-summary(data_clean)
-names(data_clean) <- c("Calories", "Age", "Gender", "Duration", "Heart_Rate", "Weight", "Height")
-
-write.csv(data_clean, "calories2.csv", row.names=FALSE)
-summary(cal)
-
-#Impute Body Temp
-#Rename variables to match
-df <- data_clean
-names(df) <- c("Calories", "Age", "Gender", "Duration", "Heart_Rate", "Weight", "Height")
-
-#Add column for Body_Temp
-df$Body_Temp <- as.numeric(NA)
-
-#Drop User_ID
-cal <- cal[, !(names(cal) == "User_ID")]
-
-#Make Gender a dummy variable
-cal$Gender <- ifelse(cal$Gender == "female", 1, 0)
-
-#Combine datasets
-combined_df <- rbind(cal, df)
-summary(combined_df)
-nrow(combined_df)
-
-# Impute missing values using kNN method
-imputed <- kNN(combined_df, k=20)
-summary(imputed$Body_Temp)
-
-combined_df[15000, ]
-combined_df[15001, ]
-
-imputed <- imputed[-(1:15000), ]
-imputed[1, ]
-
-imputed_drop <- imputed %>%
-  select(Gender, Age, Height, Weight, Duration, Heart_Rate, Body_Temp, Calories)
-
-write.csv(imputed_drop, "imputed.csv", row.names=FALSE)
-
-summary(imputed_drop)
-
-#Clean gym dataset
 gym<- read.csv("gym_members_exercise_tracking.csv")
 
+#Drop extra covariates (not included in original models)
 summary(gym)
 gym <- gym %>%
   select(Age, Gender, Weight..kg., Height..m., Avg_BPM, Session_Duration..hours., Calories_Burned)
 summary(gym)
 
+#rename to match variable names from cal
 names(gym) <- c("Age", "Gender", "Weight", "Height", "Heart_Rate", "Duration", "Calories")
 
-#Gender to binary
+#Gender to dummy variable
 gym$Gender <- ifelse(gym$Gender == "Female", 1, 0)
 gym$Gender
 
-#Height units
+#Height units to match cal
 gym$Height <- gym$Height *100
 gym$Height
 
-#Duration in minutes
+#Duration in minutes to match cal
 gym$Duration <- gym$Duration*60
 
+#Make csv
 #write.csv(gym, "gym_nonimputed.csv", row.names=FALSE)
 
+#Make dataset with imputes values for missing variable
+#Make variable column
 gym$Body_Temp <- NA
 
-#impute body temp
 #Combine datasets
 combined <- rbind(cal, gym)
 summary(combined)
@@ -94,13 +42,16 @@ nrow(combined)
 imputed1 <- kNN(combined, k=20)
 summary(imputed$Body_Temp)
 
+#Drop rows with original dataset (leave new dataset only)
 combined[15000, ]
 combined[15001, ]
 
 imputed1 <- imputed1[-(1:15000), ]
 imputed1[1, ]
 
+#Drop columns with boolean if imputed
 imputed1_drop <- imputed1 %>%
   select(Gender, Age, Height, Weight, Duration, Heart_Rate, Body_Temp, Calories)
 
-write.csv(imputed1_drop, "gym_imputed.csv", row.names=FALSE)
+#Make csv
+#write.csv(imputed1_drop, "gym_imputed.csv", row.names=FALSE)
